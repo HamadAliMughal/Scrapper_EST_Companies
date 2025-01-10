4b408937869145a60922e6b1a40045ef2843c8a1
Hamad Ali <2020cs152@student.uet.edu.pk>
2025-01-10 17:19:03 +0500

    please

--- scrapper_est/scrapper_est/settings.py
+++ scrapper_est/scrapper_est/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'scrapper_est.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / ''],
+        'DIRS': [BASE_DIR / '/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
