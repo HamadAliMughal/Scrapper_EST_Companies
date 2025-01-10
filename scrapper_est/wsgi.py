808cb6101146ca816922655d47f1bdf60b73e5ed
Hamad Ali <2020cs152@student.uet.edu.pk>
2025-01-10 18:10:14 +0500

    magum

--- scrapper_est/wsgi.py
+++ scrapper_est/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from mangum import Mangum  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapper_est.settings')
 
 application = get_wsgi_application()
+handler = Mangum(application)
